d0d7b592b28c96c9801b1bb5956177134c850b15
khushiladdi <103980069+khushiladdi@users.noreply.github.com>
2022-10-11 00:16:03 +0530

    Add files via upload

---
+++ task_5.1_code.ino
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)d
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.OUT)
+
+
+try:
+  while 1:
+    GPIO.output(10, GPIO.HIGH)
+    time.sleep(0.25)
+    GPIO.output(10, GPIO.LOW)
+    time.sleep(0.25)
+except KeyboardInterrupt:
+    GPIO.cleanup()
